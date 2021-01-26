4fd392174dd2f3f553fca7a9210a25279104c567
14360 <1436037811@qq.com>
2021-01-26 18:33:34 +0800

    compare

--- src/main/java/com/stitch/hdfs/HdfsClient.java
+++ src/main/java/com/stitch/hdfs/HdfsClient.java
@@ -20,7 +20,7 @@ public class HdfsClient {
         // 配置在集群上运行
         FileSystem fs = FileSystem.get(new URI("hdfs://150.158.174.69:9000"), configuration, "ubuntu");
         // 2 创建目录
-        fs.mkdirs(new Path("/ubuntu/output/NLine"));
+        fs.mkdirs(new Path("/wcinput"));
         // 3 关闭资源
         fs.close();
     }
@@ -29,10 +29,10 @@ public class HdfsClient {
     public void testCopyFromLocalFile() throws IOException, InterruptedException, URISyntaxException {
         // 1 获取文件系统
         Configuration configuration = new Configuration();
-        configuration.set("dfs.replication", "2");
+        configuration.set("dfs.replication", "1");
         FileSystem fs = FileSystem.get(new URI("hdfs://150.158.174.69:9000"), configuration, "ubuntu");
         // 2 上传文件
-        fs.copyFromLocalFile(new Path("e:/code_lwh/hadoop/NLineInputFormatExample/wordcount.txt"), new Path("/ubuntu/input/NLine/wordcount.txt"));
+        fs.copyFromLocalFile(new Path("d:/code/hadoop/MapreduceExample/wordcount.txt"), new Path("/wcinput/input.txt"));
         // 3 关闭资源
         fs.close();
         System.out.println("over");
@@ -61,7 +61,7 @@ public class HdfsClient {
         FileSystem fs = FileSystem.get(new
                 URI("hdfs://150.158.174.69:9000"), configuration, "ubuntu");
         // 2 执行删除
-        fs.delete(new Path("/ubuntu/output/wordcount/part-r-00000"), true);
+        fs.delete(new Path("/wcoutput"), true);
         // 3 关闭资源
         fs.close();
     }
